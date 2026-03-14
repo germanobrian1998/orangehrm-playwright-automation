9e945c09d244757583e37cf4b293893ddeeb8e5f
Brian Germano <briangermano1998@gmail.com>
2026-03-14 19:46:27 -0300

    Improve navigation stability for add employee test

--- src/pages/DashboardPage.ts
+++ src/pages/DashboardPage.ts
@@ -1,55 +1,46 @@
-import { Page, Locator } from '@playwright/test'
+import { Page, Locator } from "@playwright/test";
 
 export class DashboardPage {
-
-  readonly page: Page
-  readonly pimMenu: Locator
-  readonly addEmployeeButton: Locator
-  readonly firstNameInput: Locator
-  readonly lastNameInput: Locator
-  readonly saveButton: Locator
+  readonly page: Page;
+  readonly pimMenu: Locator;
+  readonly addEmployeeButton: Locator;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly saveButton: Locator;
 
   constructor(page: Page) {
+    this.page = page;
 
-    this.page = page
+    this.pimMenu = page.locator('a[href*="viewPimModule"]');
 
-    this.pimMenu = page.locator('a[href*="viewPimModule"]')
+    this.addEmployeeButton = page.getByRole("button", { name: "Add" });
 
-    this.addEmployeeButton = page.getByRole('button', { name: 'Add' })
+    this.firstNameInput = page.getByPlaceholder("First Name");
 
-    this.firstNameInput = page.getByPlaceholder('First Name')
+    this.lastNameInput = page.getByPlaceholder("Last Name");
 
-    this.lastNameInput = page.getByPlaceholder('Last Name')
-
-    this.saveButton = page.getByRole('button', { name: 'Save' })
-
+    this.saveButton = page.getByRole("button", { name: "Save" });
   }
 
   async goToPIM() {
+    await this.page.goto("/web/index.php/dashboard/index");
 
-    await this.page.goto('/web/index.php/dashboard/index')
+    await this.pimMenu.waitFor({ state: "visible" });
 
-    await this.pimMenu.waitFor({ state: 'visible' })
-
-    await this.pimMenu.click()
-
+    await this.pimMenu.click();
   }
 
   async addEmployee(employee: any) {
+    await this.addEmployeeButton.click();
 
-    await this.addEmployeeButton.click()
+    await this.firstNameInput.waitFor();
 
-    await this.firstNameInput.waitFor()
+    await this.firstNameInput.fill(employee.firstName);
 
-    await this.firstNameInput.fill(employee.firstName)
+    await this.lastNameInput.fill(employee.lastName);
 
-    await this.lastNameInput.fill(employee.lastName)
+    await this.saveButton.click();
 
-    await Promise.all([
-      this.page.waitForURL(/viewPersonalDetails/),
-      this.saveButton.click()
-    ])
-
+    await this.page.waitForURL(/viewPersonalDetails/, { timeout: 60000 });
   }
-
 }
